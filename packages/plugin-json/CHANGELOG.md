cdf9408b8dd28c09d5aa1ae1242c0768e7a27c5b
liangchaofei <chaofei_liang@126.com>
2023-07-28 14:11:46 +0800

    feat:  6

--- packages/plugin-json/CHANGELOG.md
+++ packages/plugin-json/CHANGELOG.md
@@ -1,5 +1,11 @@
 # umi-plugin-demo
 
+## 0.0.6
+
+### Patch Changes
+
+- qw
+
 ## 0.0.5
 
 ### Patch Changes
--- packages/plugin-json/src/index.ts
+++ packages/plugin-json/src/index.ts
@@ -3,7 +3,8 @@ import { IApi } from 'umi';
 
 export default function(api: IApi, options: any) {
     api.onStart(() => {
-      console.log('--------custom1222--------------------');
+      console.log('--------custom1222444--------------------');
+      console.log('dddd')
     });
     api.describe(({
       key: 'custom',
